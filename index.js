37bf594cb9b8796f4f7f2e48dc7222acd4821e64
Manish Gupta <manishgupta1998mus@gmail.com>
2024-06-18 01:09:57 +0530

    update changes

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV ==='production'){
-app.use(express.static(path.join(__dirname1,'/public/build')));
-app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname1,"public","build","index.html"));
-});
-}
+
 
 
 mongoose
